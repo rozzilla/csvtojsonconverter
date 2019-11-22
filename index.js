438ce34367d5a20fc8d8d643838e8c3197c2c575
Roberto Bianchi <roberto.bianki@gmail.com>
2019-11-22 12:17:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const csvtojson = require('csvtojson');
+
+const uniqueid = new Date().valueOf();
+const INPUTFILE = path.join(__dirname, 'customer-data.csv');
+const OUTPUTFILE = path.join(__dirname, `${uniqueid}.json`);
+
+csvtojson().fromFile(INPUTFILE).then((jsonres) => {
+	fs.writeFileSync(OUTPUTFILE, JSON.stringify(jsonres), (error) => {
+		if (error) {
+			console.log(error.message);
+			return false;
+		}
+	});
+});
+
+console.log(`File correctly converted from csv (${INPUTFILE}) to json (${OUTPUTFILE})`);
